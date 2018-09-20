17b76109a86e7f4cf2478ec9f44d53d12ae8d565
Maxime Chabert <mchabert@sqli.com>
2018-09-20 15:54:32 +0200

    Many fix

--- src/index.js
+++ src/index.js
@@ -44,26 +44,28 @@ function beginParty(msg) {
 
 function waitFirstNumber(msg) {
     const party = AllParty[Party.waitFirstNumberOf(AllParty, msg.author.id)];
-    const enjeu = `On rappelle l'enjeu en cas de défaite: ${party.defi} :O`
+    const enjeu = `On rappelle l'enjeu en cas de défaite: ${party.defi} :O`;
     if (isNaN(msg.content)) {
         msg.reply( 'Un nombre petit con!');
     } else if(msg.content > 10) {
         party.number = msg.content;
-        party.state = "WAIT_NUMBERS"
+        party.state = "WAIT_NUMBERS";
         msg.reply('Pas de grosse ball mais ça se joue! Envoyez moi votre chiffre par mp ;) ' + enjeu);
     } else {
         party.number = msg.content;
-        party.state = "WAIT_NUMBERS"
+        party.state = "WAIT_NUMBERS";
         msg.reply('Le courage à l\'état pur ! Envoyez moi votre chiffre par mp ;) ' + enjeu);
     }
 }
 
 function waitNumbers(msg) {
     const party = AllParty[Party.waitNumbers(AllParty, msg.author.id)];
+    console.log(party.number);
+    console.log(msg.content);
     if (isNaN(msg.content)) {
         msg.reply('Un nombre petit con!');
     }
-    else if (msg.content > party.number) {
+    else if (parseInt(msg.content) > parseInt(party.number)) {
         client.channels.get(party.channel).send(`<@${msg.author.id}> essaie de tricher et donne un chiffre hors limite :O, on attends tous qu'il recommence...!`);
     } else if(!party.response) {
         party.response = msg.content;
@@ -72,13 +74,15 @@ function waitNumbers(msg) {
         if(party.response === msg.content) {
             client.channels.get(party.channel).send(`<@${party.adversary}> est dans le mal, les deux ont donné un ${msg.content}. Il doit respecter le défi : ${party.defi} `);
             party.state = "END";
+            AllParty.splice(Party.waitNumbers(AllParty, msg.author.id), 1);
         } else {
             if(party.number <= 10) {
                 party.state = "COUNTER";
-                party.response = null;
                 client.channels.get(party.channel).send(` un ${msg.content} et un ${party.response} ça ne colle pas ! Mais <@${party.adversary}> a le droit à un contre car il a porté ses couilles !! J'attends vos nouveaux nombres en MP!`);
+                party.response = null;
             } else {
                 party.state = "END";
+                AllParty.splice(Party.waitNumbers(AllParty, msg.author.id), 1);
                 client.channels.get(party.channel).send(`<@${party.adversary}> s'en sort bien ! un ${msg.content} et un ${party.response} ça ne colle pas ! `);
             }
         }
@@ -91,7 +95,7 @@ function waitNumbersInCounter(msg) {
     if (isNaN(msg.content)) {
         msg.reply('Un nombre petit con!');
     }
-    else if (msg.content > party.number) {
+    else if (parseInt(msg.content) > parseInt(party.number)) {
         client.channels.get(party.channel).send(`<@${msg.author.id}> essaie de tricher et donne un chiffre hors limite :O, on attends tous qu'il recommence...!`);
     } else if(!party.response) {
         party.response = msg.content;
@@ -100,9 +104,11 @@ function waitNumbersInCounter(msg) {
         if(party.response === msg.content) {
             client.channels.get(party.channel).send(`<@${party.owner}> est dans le mal, les deux ont donné un ${msg.content} lors du contre. Il doit respecter le défi : ${party.defi} `);
             party.state = "END";
+            AllParty.splice(Party.waitNumbers(AllParty, msg.author.id), 1);
         } else {
                 party.state = "END";
-                client.channels.get(party.channel).send(`<@${party.owner}> s'en sort bien, le contre aurait pu faire mal ! un ${msg.content} et un ${party.response} ça ne colle pas ! `);
+            AllParty.splice(Party.waitNumbers(AllParty, msg.author.id), 1);
+            client.channels.get(party.channel).send(`<@${party.owner}> s'en sort bien, le contre aurait pu faire mal ! un ${msg.content} et un ${party.response} ça ne colle pas ! `);
         }
     }
 }
