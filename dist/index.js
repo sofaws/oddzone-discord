43e410f11481fc9cc99123103f7cd0f4cf8dbde5
Maxime Chabert <mchabert@sqli.com>
2018-09-25 17:06:07 +0200

    Push dist for heroku

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
-var _Party = require('./Party');
+var _Party = require("./Party");
 
 var _Party2 = _interopRequireDefault(_Party);
+
+var _StringUtils = require("src/utils/StringUtils");
+
+var _StringUtils2 = _interopRequireDefault(_StringUtils);
+
+var _string = require("src/constants/string");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -10,7 +16,7 @@ var Discord = require("discord.js");
 var client = new Discord.Client();
 
 client.on('ready', function () {
-    console.log('Logged in as ' + client.user.tag + '!');
+    console.log("Logged in as " + client.user.tag + "!");
 });
 
 var AllParty = [];
@@ -31,33 +37,33 @@ client.on('message', function (msg) {
 
 function beginParty(msg) {
     if (_Party2.default.VerifyAdversary(msg.mentions)) {
-        var party = new _Party2.default(msg.author.id, msg.mentions.users.values().next().value.id, msg.content.split('<@' + msg.mentions.users.values().next().value.id + '>')[1], msg.channel.id);
+        var party = new _Party2.default(msg.author.id, msg.mentions.users.values().next().value.id, msg.content.split("<@" + msg.mentions.users.values().next().value.id + ">")[1], msg.channel.id);
         if (party.ifAlreadyOnParty(AllParty)) {
-            msg.reply('Toi ou/et ton adversaire avez d\xE9ja une partie en cours ! <@' + party.adversary + '>');
+            msg.reply(_StringUtils2.default.format(_StringUtils2.default.random(_string.BEGIN_PARTY.ALREADY_IN_GAME), { adversary: party.adversary }));
         } else if (!party.defi) {
-            msg.reply('C\'est courageux de jouer, avec un defi c\'est mieux ;)');
+            msg.reply(_StringUtils2.default.random(_string.BEGIN_PARTY.WITHOUT_DEFI));
         } else {
             AllParty.push(party);
-            msg.reply('La partie commence ! Donne un chiffre <@' + party.adversary + '>');
+            msg.reply(_StringUtils2.default.format(_StringUtils2.default.random(_string.BEGIN_PARTY.GIVE_NUMBER), { adversary: party.adversary }));
         }
     } else {
-        msg.reply('Impossible de jouer avec ce joueur !');
+        msg.reply(_StringUtils2.default.random(_string.BEGIN_PARTY.IMPOSSIBLE));
     }
 }
 
 function waitFirstNumber(msg) {
     var party = AllParty[_Party2.default.waitFirstNumberOf(AllParty, msg.author.id)];
-    var enjeu = 'On rappelle l\'enjeu en cas de d\xE9faite: ' + party.defi + ' :O';
+    var enjeu = _StringUtils2.default.format(_StringUtils2.default.random(_string.WAIT_FIRST_NUMBER.ENJEU), { defi: party.defi });
     if (isNaN(msg.content)) {
-        msg.reply('Un nombre petit con!');
+        msg.reply(_StringUtils2.default.random(_string.COMMONS.A_NUMBER));
     } else if (msg.content > 10) {
         party.number = msg.content;
         party.state = "WAIT_NUMBERS";
-        msg.reply('Pas de grosse ball mais ça se joue! Envoyez moi votre chiffre par mp ;) ' + enjeu);
+        msg.reply(_StringUtils2.default.random(_string.WAIT_FIRST_NUMBER.NOT_LITTLE_NUMBER) + enjeu);
     } else {
         party.number = msg.content;
         party.state = "WAIT_NUMBERS";
-        msg.reply('Le courage à l\'état pur ! Envoyez moi votre chiffre par mp ;) ' + enjeu);
+        msg.reply(_string.WAIT_FIRST_NUMBER.LITTLE_NUMBER + enjeu);
     }
 }
 
@@ -66,26 +72,26 @@ function waitNumbers(msg) {
     console.log(party.number);
     console.log(msg.content);
     if (isNaN(msg.content)) {
-        msg.reply('Un nombre petit con!');
+        msg.reply(_StringUtils2.default.random(_string.COMMONS.A_NUMBER));
     } else if (parseInt(msg.content) > parseInt(party.number)) {
-        client.channels.get(party.channel).send('<@' + msg.author.id + '> essaie de tricher et donne un chiffre hors limite :O, on attends tous qu\'il recommence...!');
+        client.channels.get(party.channel).send(_StringUtils2.default.format(_StringUtils2.default.random(_string.COMMONS.CHEAT), { author: msg.author.id }));
     } else if (!party.response) {
         party.response = msg.content;
-        client.channels.get(party.channel).send('Et de un !');
+        client.channels.get(party.channel).send(_StringUtils2.default.random(_string.WAIT_NUMBERS.ONE_NUMBER_GIVE));
     } else {
         if (party.response === msg.content) {
-            client.channels.get(party.channel).send('<@' + party.adversary + '> est dans le mal, les deux ont donn\xE9 un ' + msg.content + '. Il doit respecter le d\xE9fi : ' + party.defi + ' ');
+            client.channels.get(party.channel).send(_StringUtils2.default.format(_StringUtils2.default.random(_string.WAIT_NUMBERS.WIN), { adversary: party.adversary, defi: party.defi }));
             party.state = "END";
             AllParty.splice(_Party2.default.waitNumbers(AllParty, msg.author.id), 1);
         } else {
             if (party.number <= 10) {
                 party.state = "COUNTER";
-                client.channels.get(party.channel).send(' un ' + msg.content + ' et un ' + party.response + ' \xE7a ne colle pas ! Mais <@' + party.adversary + '> a le droit \xE0 un contre car il a port\xE9 ses couilles !! J\'attends vos nouveaux nombres en MP!');
+                client.channels.get(party.channel).send(_StringUtils2.default.format(_StringUtils2.default.random(_string.WAIT_NUMBERS.LOSE_BUT_COUNTER), { content: msg.content, response: party.response, adversary: party.adversary }));
                 party.response = null;
             } else {
                 party.state = "END";
                 AllParty.splice(_Party2.default.waitNumbers(AllParty, msg.author.id), 1);
-                client.channels.get(party.channel).send('<@' + party.adversary + '> s\'en sort bien ! un ' + msg.content + ' et un ' + party.response + ' \xE7a ne colle pas ! ');
+                client.channels.get(party.channel).send(_StringUtils2.default.format(_StringUtils2.default.random(_string.WAIT_NUMBERS.LOSE), { content: msg.content, response: party.response, adversary: party.adversary }));
             }
         }
     }
@@ -94,21 +100,21 @@ function waitNumbers(msg) {
 function waitNumbersInCounter(msg) {
     var party = AllParty[_Party2.default.waitNumbersInCounter(AllParty, msg.author.id)];
     if (isNaN(msg.content)) {
-        msg.reply('Un nombre petit con!');
+        msg.reply(_StringUtils2.default.random(_string.COMMONS.A_NUMBER));
     } else if (parseInt(msg.content) > parseInt(party.number)) {
-        client.channels.get(party.channel).send('<@' + msg.author.id + '> essaie de tricher et donne un chiffre hors limite :O, on attends tous qu\'il recommence...!');
+        client.channels.get(party.channel).send(_StringUtils2.default.format(_StringUtils2.default.random(_string.COMMONS.CHEAT), { author: msg.author.id }));
     } else if (!party.response) {
         party.response = msg.content;
-        client.channels.get(party.channel).send('Et de un pour le contre!');
+        client.channels.get(party.channel).send(_StringUtils2.default.random(_string.WAIT_NUMBERS_COUNTER.ONE_NUMBER_GIVE));
     } else {
         if (party.response === msg.content) {
-            client.channels.get(party.channel).send('<@' + party.owner + '> est dans le mal, les deux ont donn\xE9 un ' + msg.content + ' lors du contre. Il doit respecter le d\xE9fi : ' + party.defi + ' ');
+            client.channels.get(party.channel).send(_StringUtils2.default.format(_StringUtils2.default.random(_string.WAIT_NUMBERS_COUNTER.WIN), { content: msg.content, owner: party.owner, defi: party.defi }));
             party.state = "END";
             AllParty.splice(_Party2.default.waitNumbers(AllParty, msg.author.id), 1);
         } else {
             party.state = "END";
             AllParty.splice(_Party2.default.waitNumbers(AllParty, msg.author.id), 1);
-            client.channels.get(party.channel).send('<@' + party.owner + '> s\'en sort bien, le contre aurait pu faire mal ! un ' + msg.content + ' et un ' + party.response + ' \xE7a ne colle pas ! ');
+            client.channels.get(party.channel).send(_StringUtils2.default.format(_StringUtils2.default.random(_string.WAIT_NUMBERS_COUNTER.LOSE), { content: msg.content, owner: party.owner, response: party.response }));
         }
     }
 }
